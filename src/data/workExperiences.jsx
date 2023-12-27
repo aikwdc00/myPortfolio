ae4e319d0ad17307c4a17f776c8b2d07f46a284b
FredLi <aikwdc00@hotmail.com>
2023-12-26 22:06:55 -0500

    collapse

--- src/data/workExperiences.jsx
+++ src/data/workExperiences.jsx
@@ -1,4 +1,4 @@
-export const workExperiences = [
+export const workSkills = [
   {
     title: "Software Development",
     description: `Experienced in functional: Javascript, Typescript, JQuery, Git, Docker, Webpack, Figma`,
@@ -97,6 +97,45 @@ export const workExperiences = [
         name: "React-Native",
         icon: `React`,
       },
+    ],
+  },
+];
+
+
+export const workExperiences = [
+  {
+    title: "Mobile Application Developer @ FUNPODIUM Inc.",
+    duration: "May 2021 - Present",
+    location: "Taiwan, Taipei",
+    link: "https://www.funpodium.net",
+    description: `Developing front-end and mobile applications solutions (B2C, B2B) in gaming tech on React-Native/Next.js framework. Collaborated closely with cross-functional team members to conceptualize, design, and implement innovative features.`,
+    logo: "FUNPODIUM",
+    relational: [
+      "Javascript",
+      "JQuery",
+      "React",
+      "React-Native",
+      "Redux",
+      "Node.js",
+      "Docker",
+      "Figma",
+    ],
+  },
+  {
+    title: "Front-end Developer @ Gamma Interactive Inc.",
+    duration: "2019 – 2022",
+    location: "Philippine, Remote",
+    link: "https://giiph.com/index",
+    description: `Developed the UI and UX for gaming platforms using modular component structures.`,
+    logo: "GAMMA",
+    relational: [
+      "Javascript",
+      "JQuery",
+      "React",
+      "Next.js",
+      "React-Native",
+      "Redux",
+      "Trello",
     ],
   },
 ];
--- src/pages/Home/WorkExperience/index.jsx
+++ src/pages/Home/WorkExperience/index.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import {Link} from "react-router-dom";
 import { useSelector } from "react-redux";
 import useScrollTo from "@customHook/useScrollTo";
 
 import {motion} from "framer-motion";
 
 // components
-import CustomImage from "@components/CustomImage";
 import Card from "./components/Card";
 
 // data
-import {workExperiences} from "@Data/workExperiences";
+import { workSkills, workExperiences } from "@Data/workExperiences";
 
 const variants = {
   onscreen: {
@@ -37,13 +37,31 @@ const contentVariants = {
   }
 };
 
-const cardVariants = {
-  onscreen: {
-    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
-  },
-  offscreen: {
-    transition: { staggerChildren: 0.05, staggerDirection: -1 }
-  }
+// 定义两个路径，一个为 "-"，一个为 "+"
+const pathVariants = {
+  open: { // "+" symbol
+    d: "M2 12h20M12 2l0 20",
+    transition:{duration: 0.3 },
+   },
+  closed: { // "-" symbol
+    d: "M2 12h20",
+    transition:{duration: 0.3 },
+  }, 
+};
+
+const Path = props => {
+  const toggleTheme = useSelector(state => state.theme);
+  return (
+    <motion.path
+      fill="transparent"
+      strokeWidth="2"
+      stroke={toggleTheme?.isDark?`#fff`:"hsl(0, 0%, 18%)"}
+      strokeLinecap="round"
+      variants={pathVariants}
+      animate={props.isMinus ? 'closed' : 'open'}
+      {...props}
+    />
+  )
 };
 
 function WorkExperience() {
@@ -51,6 +69,14 @@ function WorkExperience() {
   const { currentTheme, isDark } = toggleTheme;
 
   const { targetRef } = useScrollTo();
+
+  // 设置初始状态，true 为显示 "-"，false 为显示 "+"
+  const [collapse,setCollapse] = useState(0);
+
+  // 切换状态的函数
+  const togglePath = (val) => {
+    setCollapse(val)
+  };
 
   return (
     <div className={`sectionArea WeContainer`}>
@@ -76,10 +102,95 @@ function WorkExperience() {
           viewport={{ once: true }}
         >
           {/* card */}
-          {workExperiences.map((item, index) => <Card key={index} item={item} isDark={isDark} variants={variants} contentVariants={contentVariants} currentTheme={currentTheme} />)}
+          {workSkills.map((item, index) => <Card key={index} item={item} isDark={isDark} variants={variants} contentVariants={contentVariants} currentTheme={currentTheme} />)}
         </motion.div>
 
         {/* work experiences */}
+        <div className="WeContainer-experience">
+          {
+            workExperiences.map((item, index) => {
+              return (
+                <div key={index} className="WeContainer-experience-collapse">
+                  {/* head */}
+                  <div
+                    className="WeContainer-experience-collapse-head"
+                    onClick={()=>togglePath(index)}
+                  >
+                    <h4 className={`dark-color`}>{item.title}</h4>
+
+                    <div className="WeContainer-experience-collapse-head-toggleBtn">
+                      <h4 className={`dark-color`}>{item.duration}</h4>
+                      {/* toggle icon */}
+                      <div className="WeContainer-experience-collapse-head-toggleBtn-icon">
+                        <svg width="25" height="25">
+                          <motion.path
+                            fill="transparent"
+                            strokeWidth="3"
+                            stroke={`#7300FE`}
+                            strokeLinecap="round"
+                            variants={pathVariants}
+                            animate={(collapse === index) ? "closed" : "open"}
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* body */}
+                  {
+                    collapse === index ? (
+                      <motion.div
+                        className={`WeContainer-experience-collapse-body ${
+                          isDark ? `isDark` : `light-bg`
+                        }`}
+                      >
+                        {/* left */}
+                        <div className={`WeContainer-experience-collapse-body-left`}>
+                          <div className="locationAndLink">
+                            <div className="locationWrap">
+                              <p className={`${currentTheme}-color`}>
+                                {item.location}
+                              </p>
+                            </div>
+                            <div className="linkWrap">
+                              <Link 
+                                className={`${currentTheme}-color`}
+                                to={item.link}
+                              >
+                                {item.link}
+                              </Link>
+                            </div>
+                          </div>
+
+                          <p className={`${currentTheme}-color description`}>
+                            {item.description}
+                          </p>
+
+                          <motion.div 
+                            className="relational"
+                          >
+                            {item.relational.map((rel, ind) => (
+                              <p 
+                                key={ind} 
+                                className={`${currentTheme}-color`}
+                              >
+                                {rel}
+                              </p>
+                            ))}
+                          </motion.div>
+                        </div>
+
+                        {/* right */}
+                        <div></div>
+                      </motion.div>
+                    ):null
+                  }
+                </div>
+              );
+            })
+          }
+
+        </div>
       </motion.div>
     </div>
   );
